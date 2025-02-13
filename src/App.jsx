d3c2a237575b2bc0035d8e34e7cb5898468ac31a
Alex <atlexeide@gmail.com>
2025-02-13 09:58:12 +0100

    Add user component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import "./App.css";
-import CatFacts from "./CatFacts";
+import Users from "./Users";
+// import CatFacts from "./CatFacts";
 // import CookieClicker from "./CookieClicker";
 
 function App() {
-  return <CatFacts />;
+  return <Users />;
 }
 
 export default App;
