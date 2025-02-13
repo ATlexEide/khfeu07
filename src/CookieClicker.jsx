2078e6debd08d7bb1ac7af9c1802aa8d6ff91399
Alex <atlexeide@gmail.com>
2025-02-13 09:25:51 +0100

    Add cookieClicker solution

--- src/CookieClicker.jsx
+++ src/CookieClicker.jsx
@@ -2,10 +2,30 @@ import { useState } from "react";
 
 function CookieClicker() {
   const [count, setCount] = useState(0);
+  const [upgradeCount, setUpgradeCount] = useState(0);
+  const upgradePrice = 10;
+  10;
   return (
     <>
       <h1>{count} cookies</h1>
-      <button onClick={setCount(count + 1)}>Click me</button>
+      <h2>You have {upgradeCount} upgrades</h2>
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        Click me
+      </button>
+      <button
+        onClick={() => {
+          if (count >= upgradePrice) {
+            setCount(count - upgradePrice);
+            setUpgradeCount(upgradeCount + 1);
+          }
+        }}
+      >
+        Buy some upgrade
+      </button>
     </>
   );
 }
