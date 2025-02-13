e9859d0fe2e099256aa78136e4651002bea55724
Alex <atlexeide@gmail.com>
2025-02-13 09:54:51 +0100

    Add error and loading state

--- src/CatFacts.jsx
+++ src/CatFacts.jsx
@@ -2,18 +2,23 @@ import { useEffect, useState } from "react";
 function CatFacts() {
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState("");
+
   useEffect(() => {
     fetch("https://catfact.ninja/facts?limit=5")
       .then((res) => res.json())
       .then((res) => setData(res.data))
       .then(setIsLoading(false))
-      .catch((err) => console.log(err));
+      .catch((err) => setError(err));
   }, []);
+
   console.log(data);
+  console.table(error);
   return (
     <>
       <h1>Cat facts</h1>
-      {isLoading ? (
+      {error.message && <p>{error.message}</p>}
+      {isLoading && !error.message ? (
         <p>Loading...</p>
       ) : (
         <ul>
